c1d8cd81979ed499c6cfe2a59b162c738a76c87f
oljung <patrik_osterljung@hotmail.com>
2022-03-06 10:16:32 +0000

    set up initial index page and urls

---
+++ home/views.py
@@ -0,0 +1,11 @@
+"""
+views for the boutique app
+"""
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ Renders the homepage """
+    return render(request, "index.html")
