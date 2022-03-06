dc6e5526879e2986cf5f5b47a07eb6a77b484295
oljung <patrik_osterljung@hotmail.com>
2022-03-06 14:22:36 +0000

    create category and produc models and loaddata from fixtures

---
+++ products/models.py
@@ -0,0 +1,43 @@
+""" Module containing product models """
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    """ Model for category """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ Returns the name """
+        return f'{self.name}'
+
+    def get_fiendly_name(self):
+        """ Return user friendly name """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """ Model for products """
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """ Returns the name """
+        return f'{self.name}'
