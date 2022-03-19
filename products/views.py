ee2a426964e6a84e0b4646b092456286d523ef00
oljung <patrik_osterljung@hotmail.com>
2022-03-19 13:29:02 +0000

    add product details page and functionality

--- products/views.py
+++ products/views.py
@@ -1,14 +1,14 @@
 """
 views for the boutique app
 """
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all product, with search and filter """
+    """ A view to show the product details of a selected product """
 
     products = Product.objects.all()
 
@@ -17,3 +17,15 @@ def all_products(request):
     }
 
     return render(request, "products.html", context)
+
+
+def product_detail(request, product_id):
+    """ A view to show all product, with search and filter """
+
+    product = get_object_or_404(Product, id=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, "product_detail.html", context)
