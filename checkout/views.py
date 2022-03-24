e9b740e40533fa7d1250a3815fb0b910b374bfc7
oljung <patrik_osterljung@hotmail.com>
2022-03-24 09:23:57 +0000

    add stripe card element

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Kgmb8DQdQuXgEDRtwP2FUw3bzW4KVqyUo0YEyWHwzQUIcBx2V6htInzwxOARMk0dxBNJtp6FxFz4qc9SHvZfOef001odCBgTq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
